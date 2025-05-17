a6d1c31b707604e1ff435f4570e63d17e1989033
YoumsCo <youmsc.co@gmail.com>
2025-05-17 19:45:00 +0100

    modified:   app/models/Database.php 	modified:   vendor/composer/installed.php

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,29 +1,37 @@
 <?php
 
+require_once __DIR__."/../../vendor/autoload.php";
+use Dotenv\Dotenv;
 
-    class Database {
-        private $dsn;
-        private const server = $_ENV["SERVER"];
-        private const dbname = $_ENV["DB_NAME"];
-        private const port = $_ENV["PORT"];
-        private const user = $_ENV["USER"];
-        private const password = $_ENV["PASSWORD"];
-        private $pdo;
+class Database
+{
+    private $pdo;
 
-        function __construct() {
-            $this->dsn = "mysql:host=".self::server."; dbname=".self::dbname."; port=".self::port."; charset=utf8";
-            
-            try {
-                $this->pdo = new PDO($this->dsn, self::user, self::password);
-                $this->pdo -> setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
-                $this->pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-                $this->pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }catch(PDOException $e) {
-                die("Connexion echouée ". $e->getMessage() .". A la ligne : ". $e->getLine(). ", du fichier : ". $e->getFile());
-            }
-        }
-        
-        function returnPDO() {
-            return $this->pdo;
+    function __construct()
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__."/../../");
+        $dotenv->load();
+
+        $server = $_ENV["SERVER"];
+        $dbname = $_ENV["DB_NAME"];
+        $port = $_ENV["PORT"];
+        $user = $_ENV["USER"];
+        $password = $_ENV["PASSWORD"];
+
+        $dsn =(string) "mysql:host=" . $server . "; dbname=" . $dbname . "; port=" . $port . "; charset=utf8";
+
+        try {
+            $this->pdo = new PDO($dsn, $user, $password);
+            $this->pdo->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connexion echouée " . $e->getMessage() . ". A la ligne : " . $e->getLine() . ", du fichier : " . $e->getFile());
         }
     }
+
+    function returnPDO()
+    {
+        return $this->pdo;
+    }
+}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e63bdd5ece2dbe4139616ff15044c7cc98b030ee',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e63bdd5ece2dbe4139616ff15044c7cc98b030ee',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
